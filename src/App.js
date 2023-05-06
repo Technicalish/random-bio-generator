016ab98b06faa53fb746149371b5daf1cd59cae5
Technicalish <66911671+Technicalish@users.noreply.github.com>
2023-05-06 12:56:51 +0530

    ak

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Main from "./Main";
 import Footer from "./Footer";
 import Chance from "chance";
+import { useLocation } from 'react-router-dom';
 var chance = new Chance();
 function App() {
+  console.log(useLocation())
   return (
     <>
     <Main name={chance.name()} aboutme={chance.paragraph()} domain={ chance.domain({ tld: "com" }) } country={chance.country().toLowerCase()}/>
