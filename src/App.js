4e44d3367dd094e43fc8d359d0cadc6d7ff57081
Technicalish <66911671+Technicalish@users.noreply.github.com>
2023-05-06 13:12:57 +0530

    ak

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Main from "./Main";
 import Footer from "./Footer";
 import Chance from "chance";
-import { BrowserRouter, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 var chance = new Chance();
 function App() {
   var location = useLocation();
-  console.log(location);
+
+  React.useEffect(() => {
+    console.log(location)
+  }, [location]);
+
   return (
-    <BrowserRouter>
-      <>
-      <Main name={chance.name()} aboutme={chance.paragraph()} domain={ chance.domain({ tld: "com" }) } country={chance.country().toLowerCase()}/>
-      <Footer/>
-      </>
-    </BrowserRouter>
+    <>
+    <Main name={chance.name()} aboutme={chance.paragraph()} domain={ chance.domain({ tld: "com" }) } country={chance.country().toLowerCase()}/>
+    <Footer/>
+    </>
   )
 }
 export default App;
